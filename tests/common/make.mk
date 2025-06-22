2dbe1ea6a853bea44b0b181b9145e964adab4876
Eugene Hutorny <eugene@hutorny.in.ua>
2025-06-22 13:49:54 +0300

    Make build failing on warnings

--- tests/common/make.mk
+++ tests/common/make.mk
@@ -8,7 +8,7 @@
 INCLUDES = include tests/common 
 BUILDDIR = build
 CXXFLAGS += $(STD:%=-std=%) $(INCLUDES:%=-I$(PROJROOT)%) $(WARNINGS) $(if $(findstring gcc, $(CXX)), $(GCCWARN))
-WARNINGS = -pedantic -Wall -Wextra -Wconversion -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization \
+WARNINGS = -pedantic -Werror -Wall -Wextra -Wconversion -Wcast-align -Wcast-qual -Wctor-dtor-privacy -Wdisabled-optimization \
            -Wmissing-declarations -Wmissing-include-dirs  -Wold-style-cast -Woverloaded-virtual -Wredundant-decls \
            -Wshadow -Wsign-conversion -Wsign-promo -Wswitch-default -Wundef -Weffc++ -Wfloat-equal \
            -Wno-c++20-extensions -Wno-c++17-extensions
